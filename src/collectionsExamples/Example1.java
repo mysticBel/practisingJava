14089fab3a779333feff2dd446dc1977952753c3
mysticBel <dnxbel@gmail.com>
2022-06-07 19:07:14 -0500

    Initial commit

---
+++ src/collectionsExamples/Example1.java
@@ -0,0 +1,58 @@
+package collectionsExamples;
+
+import java.util.ArrayList;
+
+public class Example1 {
+    public static void main(String[] args) {
+        String variable1 = "Ejem 1";
+        String variable2 = "Ejem 2";
+        String variable3 = "Ejem 3";
+        String variable4 = "Ejem 4";
+        String variable5 = "Ejem 5";
+
+        ArrayList<String> lista = new ArrayList<>();
+
+        // adicionar un valor a la variable lista
+        lista.add(variable1);
+        lista.add(variable2);
+        lista.add(variable3);
+        lista.add(variable4);
+        lista.add(variable5);
+
+        System.out.println(lista); // [Ejem 1, Ejem 2, Ejem 3, Ejem 4, Ejem 5]
+
+        //remover
+        lista.remove(2);
+        System.out.println(lista); //[Ejem 1, Ejem 2, Ejem 4, Ejem 5]
+
+        //set-modificar
+        lista.set(2, "Ejem alterado");
+        System.out.println(lista); //[Ejem 1, Ejem 2, Ejem alterado, Ejem 5]
+
+        //size method
+        System.out.println(lista.size()); //4
+
+        // Leer un ArrayList
+        // forma (1)
+        for (int i = 0 ; i < lista.size(); i++) {
+            System.out.println(lista.get(i));  //get sirve para ubicar la posición
+        }
+        /*
+        * Ejem 1
+        * Ejem 2
+        * Ejem alterado
+        * Ejem 5
+        * */
+
+        // forma (2)
+        for(String ejemListaString: lista) {
+            System.out.println(ejemListaString);
+        }
+
+        // forma (3)
+        lista.forEach(ejem -> {
+            System.out.println(ejem);
+        });
+
+    }
+}
---
+++ src/collectionsExamples/Example2.java
@@ -0,0 +1,43 @@
+package collectionsExamples;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example2 {
+    public static void main(String[] args) {
+        String curso1 = "Magic 101";
+        String curso2 = "Programming";
+        String curso3 = "Art & Crafts";
+        String curso4 = "Mindset";
+
+        ArrayList<String> listaCursos = new ArrayList<>();
+
+        // adicionar un valor a la variable listaCursos
+        listaCursos.add(curso1);
+        listaCursos.add(curso2);
+        listaCursos.add(curso3);
+        listaCursos.add(curso4);
+
+        System.out.println(listaCursos); //[Magic 101, Programming, Art & Crafts, Mindset]
+
+        // Collections - orden de menor a mayor
+        Collections.sort(listaCursos);
+        System.out.println(listaCursos); //[Art & Crafts, Magic 101, Mindset, Programming]
+        Collections.sort(listaCursos, Collections.reverseOrder());
+        System.out.println(listaCursos); //[Programming, Mindset, Magic 101, Art & Crafts]
+
+        // ahora usando el metodo sort y Comparator(java.utils)
+        listaCursos.sort(Comparator.naturalOrder());
+        System.out.println(listaCursos); //[Art & Crafts, Magic 101, Mindset, Programming]
+        listaCursos.sort(Comparator.reverseOrder());
+        System.out.println(listaCursos); //[Programming, Mindset, Magic 101, Art & Crafts]
+
+        // Stream y sorted -a partir de Java 8
+        //listaCursos.stream().sorted().collect(Collectors.toList());
+        List<String> cursosList = listaCursos.stream().sorted().collect(Collectors.toList());
+        System.out.println(cursosList);  //[Art & Crafts, Magic 101, Mindset, Programming]
+    }
+}
